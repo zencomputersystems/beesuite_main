c6d3828f1df3d00d509f7e5c1239476cc92bdb78
Ruoli <angrl@zen.com.my>
2020-07-20 09:24:19 +0800

    Delete home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomePageModule } from './home/home.module';
 import { AppModule as App2SharedModule } from 'projects/eLeave_admin-V3/src/app/app.module';
 import { AppModule as App1SharedModule } from 'projects/eleave-v3-master/src/app/app.module';
 import { CommonModule } from '@angular/common';
@@ -16,7 +15,7 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [CommonModule, IonicModule.forRoot(), AppRoutingModule, HomePageModule,
+  imports: [CommonModule, IonicModule.forRoot(), AppRoutingModule, 
     App2SharedModule, App1SharedModule
   ],
   providers: [
