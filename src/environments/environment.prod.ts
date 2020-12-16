75c97052010b08fda4eb673661cabffa5b6e4240
Ruoli <angrl@zen.com.my>
2020-12-16 15:58:45 +0800

    Change appcore login to auth login url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   API_URL: 'https://appcore.beesuite.app',
-  FORGET_PASS_URL: 'https://resetpassword.beesuite.app'
+  FORGET_PASS_URL: 'https://resetpassword.beesuite.app',
+  AUTH_URL: 'https://auth.beesuite.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   API_URL: 'https://appcore.beesuite.app',
-  FORGET_PASS_URL: 'https://resetpassword.beesuite.app'
+  FORGET_PASS_URL: 'https://resetpassword.beesuite.app',
+  AUTH_URL: 'https://auth.beesuite.app'
 };
 
 /*
